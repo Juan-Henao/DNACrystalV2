466340cd729abbaab35fe50e4dbf658ebd5312f3
juanhenao31 <jdavidhm31@gmail.com>
2021-04-23 15:25:22 -0500

    Update

--- microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -1,10 +1,9 @@
 package com.ceiba.configuracion;
 
-import com.ceiba.puerto.repositorio.RepositorioUsuario;
-import com.ceiba.servicio.usuario.ServicioActualizarUsuario;
-import com.ceiba.servicio.usuario.ServicioCrearUsuario;
-import com.ceiba.servicio.usuario.ServicioEliminarUsuario;
-
+import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
+import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
+import com.ceiba.usuario.servicio.ServicioCrearUsuario;
+import com.ceiba.usuario.servicio.ServicioEliminarUsuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
