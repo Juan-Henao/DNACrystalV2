1b4c8b420bb1fbdb1b378db154f932e3e15c653e
juanhenao31 <jdavidhm31@gmail.com>
2021-04-23 08:39:42 -0500

    Base

--- comun/comun-dominio/src/main/java/com/ceiba/dominio/ValidadorArgumento.java
+++ comun/comun-dominio/src/main/java/com/ceiba/dominio/ValidadorArgumento.java
@@ -63,6 +63,12 @@ public class ValidadorArgumento {
         }
     }
 
+    public static void validarMayorDouble(Double numeroInicial, Double numeroFinal, String mensaje) {
+        if (numeroFinal < numeroInicial) {
+            throw new ExcepcionValorInvalido(mensaje);
+        }
+    }
+    
     public static void validarRegex(String correoElectronico, String regex, String mensaje) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(correoElectronico);
