2e5e1f663d75277dee2f88fc732af06a07cd3c91
juanhenao31 <jdavidhm31@gmail.com>
2021-04-26 02:28:51 -0500

    update itemsCompra

--- microservicio/dominio/src/main/java/com/ceiba/itemsCompra/servicio/ServicioCrearItemsCompra.java
+++ microservicio/dominio/src/main/java/com/ceiba/itemsCompra/servicio/ServicioCrearItemsCompra.java
@@ -25,15 +25,12 @@ public class ServicioCrearItemsCompra {
 
 	private final RepositorioItemsCompra repositorioItemsCompra;
 	private final DaoParametro daoParametro;
-	private final ServicioActualizarCompra servicioActualizarCompra;
-	private final DaoCompra daoCompra;
 
-	public ServicioCrearItemsCompra(RepositorioItemsCompra repositorioItemsCompra, DaoParametro daoParametro,
-			ServicioActualizarCompra servicioActualizarCompra, DaoCompra daoCompra) {
+
+	public ServicioCrearItemsCompra(RepositorioItemsCompra repositorioItemsCompra, DaoParametro daoParametro) {
 		this.repositorioItemsCompra = repositorioItemsCompra;
 		this.daoParametro = daoParametro;
-		this.servicioActualizarCompra = servicioActualizarCompra;
-		this.daoCompra = daoCompra;
+
 	}
 
 	public Long ejecutar(ItemsCompra itemsCompra) {
@@ -45,19 +42,10 @@ public class ServicioCrearItemsCompra {
 				.parseLong(daoParametro.obtenerPorEnum(EnumParametro.ITEMS_MINIMOS_DESCUENTO).getValor())) {
 			aplicarDescuento(itemsCompra);
 		}
-		cambiarValorCompra(itemsCompra);
 
 		return this.repositorioItemsCompra.crear(itemsCompra);
 	}
 
-	private void cambiarValorCompra(ItemsCompra itemsCompra) {
-		DtoCompra dtoCompra = daoCompra.obtener(itemsCompra.getIdCompra());
-		dtoCompra.setTotal(dtoCompra.getTotal()+itemsCompra.getValor());
-		
-		servicioActualizarCompra.ejecutar(
-				new Compra(dtoCompra.getId(),dtoCompra.getIdCliente(),dtoCompra.getTotal(),dtoCompra.getFechaCompra(),dtoCompra.getFechaEntrega(),dtoCompra.getEstadoCompra() ));
-				
-	}
 
 	private void validarExistenciaPrevia(ItemsCompra itemsCompra) {
 		boolean existe = this.repositorioItemsCompra.existe(itemsCompra.getFechaCreacion(), itemsCompra.getIdCompra());
